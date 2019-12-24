84b7ec12c5742fa43296a8fbf0431d27e3b0b43e
crigasro <gasparrozalen@gmail.com>
2019-12-24 16:02:31 +0100

    Initital implementation for /search

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # peliculas-api
-API REST para solicitar peliculas.
+API REST para solicitar películas.
+
+El usuario podrá solicitar un listado de películas a partir de una cadena de búsqueda mediante el endpoint `/search?chain=<lo que se quiera buscar>`.
+Ejemplo: `http://localhost:8080/search?chain=the-godfather`
+
+También podrá buscar un detalle de una película dado su ID haciendo peticiones a `/getById?id=<el id de la película>`, pasando como parámetro lo que se quiere obtener. Esto sería:
+* `&detail=title` devuelve el título de la película.
+* `&detail=description` devuelve la descripción de la película.
+* `&detail=cast` devuelve el listado de actores de la película.
+* `&detail=img` devuelve la URL de la imagen de la película.
+
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const config = require("../config/config");
 
 const API_KEY = config.apiKey;
@@ -6,8 +7,35 @@ const BASE_URL = config.baseURL;
 
 let app = express();
 
-app.get("/api", (req, res) => {
-    res.end("you requested /api\n")
+// Handle request for a list of films given a search chain 
+app.get("/search", (req, res) => {
+    let query = req.query;
+    let mbd_url = BASE_URL + "search/movie?api_key=" + API_KEY + "&query=" + query.chain;
+
+    axios.get(mbd_url)
+    .then((films) => {
+        let results = films.data.results;
+
+        results.forEach(result => {
+            let json_res = JSON.stringify(result);
+            console.log(json_res + "\n");
+            res.write(json_res + "\n");
+        });
+        
+        res.end("\n");
+    })
+    .catch((err) => {
+        console.log("An error ocurred while requesting /search:\n", err);
+        res.end("Error while requesting to The Movie DB");
+    })
+})
+
+app.get("/getById", (req, res) => {
+    let query = req.query;
+    console.log("Req query id: ", query.id);
+    console.log("Req query title: ", query.title);
+
+    res.end("/getById request handled\n");
 })
 
 app.listen(8080, () => {
