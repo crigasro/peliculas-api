630641e7e7103821d084dba981a3dd79b018c9eb
crigasro <gasparrozalen@gmail.com>
2019-12-24 19:12:56 +0100

    Added JWT protection (mocked) and changed README.md

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ También podrá buscar un detalle de una película dado su ID haciendo peticione
 * `&detail=poster` devuelve la URL de la imagen de la película.
 Ejemplo: `http://localhost:8080/getById?id=1893&detail=cast`
 
+Ambas rutas están protegidas con JWT. Se ha añadido un usario mock, que es el que se autentica cuando se hace una petición POST a /login. El token generado, que será necesario añadir a la cabecera de la petición a `/search` o `/getById`, es el siguiente:
+`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImpvaG4iLCJlbWFpbCI6ImpvaG5AZG9lLmNvbSJ9LCJpYXQiOjE1NzcyMTAzNzl9.y0IU2XkHefvwYQCyV8WQWVcRnHNRIHiHfCJ75jRVi6o`
+
+
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 
 const API_KEY = config.apiKey;
@@ -7,8 +8,20 @@ const BASE_URL = config.baseURL;
 
 let app = express();
 
+// Login will be mocked
+app.post('/login', (req, res) => {
+    const mockUser = {
+        id: 1,
+        username: "john",
+        email: "john@doe.com"
+    }
+    jwt.sign({user: mockUser}, "secretkey", (err, token) => {
+        res.json({token});
+    })
+})
+
 // Handle request for a list of movies given a search chain 
-app.get("/search", (req, res) => {
+app.get("/search", verifyToken, (req, res) => {
     let query = req.query;
     let mbd_url = BASE_URL + "search/movie?api_key=" + API_KEY + "&query=" + query.chain;
 
@@ -30,53 +43,74 @@ app.get("/search", (req, res) => {
 })
 
 // Handle request for a detail of a movie given its id
-app.get("/getById", (req, res) => {
-    let query = req.query;
-    let movie_id = query.id;
-    let movie_detail = query.detail;
-    console.log("Req query id: ", movie_id);
-    console.log("Req query detail: ", movie_detail);
-    
-    if(movie_detail == "" || movie_detail == undefined) {
-        res.end("You need to write something for detail parameter");
-    }
+app.get("/getById", verifyToken, (req, res) => {
 
-    let mbd_url = "";
-    
-    if(movie_detail != "cast") {
-        mbd_url = BASE_URL + "movie/" + movie_id + "?api_key=" + API_KEY;
-    } else {
-        mbd_url = BASE_URL + "movie/" + movie_id + "/credits?api_key=" + API_KEY;
-    }
-
-    axios.get(mbd_url)
-    .then((movie) => {
-        switch(movie_detail){
-            case "title": 
-                res.write(movie.data.original_title, null, 2);
-                break;
-            case "description":
-                res.write(movie.data.overview, null, 2);
-                break;
-            case "poster": 
-                let poster_path = "http://image.tmdb.org/t/p/w185" + movie.data.poster_path
-                res.write(poster_path, null, 2);
-                break;
-            case "cast":
-                res.write(JSON.stringify(movie.data.cast, null, 2));
-                break;
-            default:
-                res.write("Detail can only be title, description, poster or cast");
-                break;
+    jwt.verify(req.token, "secretkey", (err, data) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            let query = req.query;
+            let movie_id = query.id;
+            let movie_detail = query.detail;
+            console.log("Req query id: ", movie_id);
+            console.log("Req query detail: ", movie_detail);
+            
+            if(movie_detail == "" || movie_detail == undefined) {
+                res.end("You need to write something for detail parameter");
+            }
+        
+            let mbd_url = "";
+            
+            if(movie_detail != "cast") {
+                mbd_url = BASE_URL + "movie/" + movie_id + "?api_key=" + API_KEY;
+            } else {
+                mbd_url = BASE_URL + "movie/" + movie_id + "/credits?api_key=" + API_KEY;
+            }
+        
+            axios.get(mbd_url)
+            .then((movie) => {
+                switch(movie_detail){
+                    case "title": 
+                        res.write(movie.data.original_title, null, 2);
+                        break;
+                    case "description":
+                        res.write(movie.data.overview, null, 2);
+                        break;
+                    case "poster": 
+                        let poster_path = "http://image.tmdb.org/t/p/w185" + movie.data.poster_path
+                        res.write(poster_path, null, 2);
+                        break;
+                    case "cast":
+                        res.write(JSON.stringify(movie.data.cast, null, 2));
+                        break;
+                    default:
+                        res.write("Detail can only be title, description, poster or cast");
+                        break;
+                }
+                res.end()
+            })
+            .catch((err) => {
+                console.log("An error ocurred while requesting movie by id:\n", err);
+                res.end("Error while requesting to The Movie DB by movie id");
+            })
         }
-        res.end()
-    })
-    .catch((err) => {
-        console.log("An error ocurred while requesting movie by id:\n", err);
-        res.end("Error while requesting to The Movie DB by movie id");
-    })
+    })    
 })
 
 app.listen(8080, () => {
     console.log("Running on port 8080");
 })
+
+// Verify token
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader != "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
